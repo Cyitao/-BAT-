a827e4d1a6c27ef1f5c72c0639c15ebf5b36f6b5
Cyitao <418867269@qq.com>
2016-08-17 20:46:42 +0800

    modify

--- ch12 动态规划/01背包练习题/Backpack.cpp
+++ ch12 动态规划/01背包练习题/Backpack.cpp
@@ -1,28 +1,39 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-class Backpack {
-public:
-	int maxValue(vector<int> w, vector<int> v, int n, int cap) 
-	{
-		int **dp=new int*[n+1];
-		for(int i=0;i<n+1;i++)
-			dp[i]=new int[cap+1];
-		for(int i=0;i<n+1;i++)
-			dp[i][0]=0;
-		for(int i=0;i<cap+1;i++)
-			dp[0][i]=0;
-		for(int i=1;i<n+1;i++)
-			for(int j=1;j<cap+1;j++)
-			{
-				int temp=dp[i-1][j];
-				if(j-w[i-1]>=0)
-					temp=max(temp,dp[i-1][j-w[i-1]]+v[i-1]);
-				dp[i][j]=temp;
-			}
-			return dp[n][cap];
-	}
-};
+class Backpack  
+{  
+public:          
+	int maxValue(vector<int> w, vector<int> v, int n, int cap)           
+	{                  
+		int **dp=new int*[n];                  
+		for(int i=0;i<n;i++)                          
+			dp[i]=new int[cap+1]();                  
+		for(int i=0;i<n;i++)                          
+			dp[i][0]=0;                  
+		for(int i=0;i<cap+1;i++)                  
+		{                          
+			if(i>=w[0])                                 
+				dp[0][i]=v[0];                          
+			else                               
+				dp[0][i]=0;                  
+		}                  
+		for(int i=1;i<n;i++)                          
+			for(int j=1;j<cap+1;j++)                      
+			{                               
+				if((j-w[i])>=0)                                       
+					dp[i][j]=max(dp[i-1][j],dp[i-1][j-w[i]]+v[i]);                               
+				else                                     
+					dp[i][j]=dp[i-1][j];                      
+			}                  
+	    int res=dp[n-1][cap];                  
+	    for(int i=0;i<n;i++)                          
+			delete []dp[i];                  
+		delete []dp;                  
+		return res;          
+	}  
+}; 
+
 
 int main()
 {
